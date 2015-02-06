3599a6ab8d048047ef61ee0b56e3341514c80690
Scott Gaul <sgaul@hfpg.org>
2015-02-06 07:47:19 -0500

    Edits to text
    Clean up table format and intro text

--- plotcorr-ex.R
+++
@@ -1,8 +0,0 @@
-corr <- cor(x)
-
-corr <- corr+t(corr)
-diag(corr) <- 1
-vnames <- c("College","Public assistance","Poverty","Unemployment","Owner-occupied","Commute time","Vacancy","Job growth","Math test scores","Reading test scores")
-rownames(corr) <- colnames(corr) <- vnames
-
-plot.corr(corr)
